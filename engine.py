c2dabfa03a67cd147da9b888b1cd557d30ce229d
Mostafa Asadi <mostafaasadi73@gmail.com>
2021-05-26 02:57:43 +0430

    drawing engine

---
+++ engine.py
@@ -0,0 +1,21 @@
+# from __future__ import division
+import uuid
+import numpy as np
+from PIL import Image
+from scipy import interpolate
+import matplotlib.pyplot as plt
+
+
+def draw(tck, img):
+    x = np.linspace(0, 1, num=100, endpoint=True)
+    img = Image.open(img).convert('RGBA')
+    xnew, ynew = interpolate.splev(x, tck=tck, der=0)
+    plt.imshow(img, aspect='auto', zorder=0)
+    plt.plot(xnew, ynew, color='red', zorder=1)
+    plt.axis('off')
+    try:
+        name = uuid.uuid4().hex
+        plt.savefig('{}/{}'.format('img/generates/', name), bbox_inches='tight', pad_inches=0)
+        return(name)
+    except Exception:
+        return(False)
