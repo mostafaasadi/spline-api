e0cf29ea2469c3bfafd2282b38f25b48087242a0
Mostafa Asadi <mostafaasadi73@gmail.com>
2021-05-26 14:54:26 +0430

    some performance

--- engine.py
+++ engine.py
@@ -8,14 +8,15 @@ import matplotlib.pyplot as plt
 
 def draw(tck, img):
     x = np.linspace(0, 1, num=100, endpoint=True)
-    img = Image.open(img).convert('RGBA')
+    with Image.open(img).convert('RGBA') as img:
+        plt.imshow(img, aspect='auto', zorder=0)
     xnew, ynew = interpolate.splev(x, tck=tck, der=0)
-    plt.imshow(img, aspect='auto', zorder=0)
     plt.plot(xnew, ynew, color='red', zorder=1)
     plt.axis('off')
     try:
         name = uuid.uuid4().hex
         plt.savefig('{}/{}'.format('img/generates/', name), bbox_inches='tight', pad_inches=0)
+        plt.close()
         return('{}/{}.png'.format('img/generates', name))
     except Exception:
         return(False)
